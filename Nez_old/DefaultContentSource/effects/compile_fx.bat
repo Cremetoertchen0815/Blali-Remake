60d37718515649ec4a95af43802643f3fc2ebd81
Mirco Heck <66530591+Cremetoertchen0815@users.noreply.github.com>
2021-09-25 13:27:16 +0200

    Delete Nez_old directory

--- Nez_old/DefaultContentSource/effects/compile_fx.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "respath=C:\Users\Creme\source\repos\NezTestVB\NezTestVB\bin\Windows\AnyCPU\Debug\Content\nez\effects"
-cd "C:\Users\Creme\source\repos\Nez\Nez\DefaultContentSource\effects"
-for %%f in (*.fx) do (
-    mgfxc "%%~nf.fx" "%respath%\%%~nf.mgfxo" /Profile:OpenGL
-)
-pause
