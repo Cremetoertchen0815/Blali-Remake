59e01509175b418f5738790d26474216e6c0f918
Johannes.g.brandau@gmail.com <Johannes.g.brandau@gmail.com>
2021-09-28 18:12:14 +0200

    Finally did something (i did le Button in the Menü ) Unterschrift : Tach3

--- Blali-Remake/Menu/MainMenu.vb
+++ Blali-Remake/Menu/MainMenu.vb
@@ -1,11 +1,34 @@
-﻿Namespace Menu
+﻿
+
+Namespace Menu
+
+
     Public Class MainMenu
         Inherits Scene
+
+        Private lastmstate As MouseState
+
         Public Overrides Sub Initialize()
             AddRenderer(New DefaultRenderer)
             CreateEntity("BlaliHintergrundbildDeckmode").SetLocalPosition(New Vector2(1920 / 2, 1020 / 2)).SetScale(2).AddComponent(New Sprites.SpriteRenderer(Content.LoadTexture("toadallybestbackgwoundpnguwillevewsee")))
-            CreateEntity("Buttons").SetLocalPosition(New Vector2(1920 / 3 - 350, 1020 / 3 - 75)).AddComponent(New PrototypeSpriteRenderer(350, 50 * 3.5)) '{With color = color.blanchedalmond}
+            CreateEntity("Buttons").SetLocalPosition(New Vector2(1920 / 3 - 350, 1020 / 3 - 75)).AddComponent(New PrototypeSpriteRenderer(350, 50 * 3.5))
+
+
+        End Sub
+        Public Overrides Sub Update()
+            Dim mstate As MouseState = Mouse.GetState
+            Dim mpos As Point = Vector2.Transform(mstate.Position.ToVector2, Matrix.Invert(ScreenTransformMatrix)).ToPoint
+            Dim OneshotPressed As Boolean = mstate.LeftButton = ButtonState.Pressed And lastmstate.LeftButton = ButtonState.Released
+
+            If mpos.X > 115 And mpos.Y > 177 AndAlso mpos.X < 463 And mpos.Y < 350 AndAlso OneshotPressed = True Then
+                Core.Scene = New Games.Blali_1.Remastered.GameScene(0)
+            End If
+            'If OneshotPressed = True Then
+            '    System.Console.WriteLine("Tach3 because why not xP !1!!")
+            'End If
+            MyBase.Update()
         End Sub
 
     End Class
+
 End Namespace
